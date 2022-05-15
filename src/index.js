ab6fc3f3d1f282b748c8fb5cff50a7328e3aed12
Ganesh Patil <ganeshpatil0101@gmail.com>
2022-05-15 18:09:23 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -3,12 +3,8 @@ import { Test } from "./examples/example1/example1";
 import { PromiseTest } from "./examples/promise/promiseTest";
 
 document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
+<h1>Js examples</h1>
+<div></div>
 `;
 
 Test(9);
