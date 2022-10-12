f850b880ce214147fd99dde58e1c39c35f3468cf
Ganesh Patil <ganeshpatil0101@gmail.com>
2022-10-12 22:09:08 +0530

    added loader while fetch

--- eventSource/eventSourceExample2.js
+++ eventSource/eventSourceExample2.js
@@ -6,6 +6,7 @@ const EURL = (location.host === 'localhost:1234') ? '//localhost:3000/esource' :
 sendApiCall.addEventListener('click', () => {
     // send api call
     addLogs('Requesting server to process large data');
+    document.body.style.cursor = 'wait';
     fetch(URL).then((res) => {
         return res.json();
     }).then((result) => {
@@ -13,16 +14,17 @@ sendApiCall.addEventListener('click', () => {
         // Event Source Object
         let eventStream = new EventSource(`${EURL}`);
         eventStream.addEventListener('open', () => {
-            document.body.style.cursor = 'default';
             console.log("Connected and subscribed to channel ");
         });
         eventStream.addEventListener('message', (event) => {
             const data = JSON.parse(event.data);
             console.log(' eventStream data', data);
             addLogs(data.msg);
+            document.body.style.cursor = 'default';
             eventStream.close();
         });
         eventStream.addEventListener('error', (err) => {
+            document.body.style.cursor = 'default';
             console.error("EventSource failed:", err);
         });
     });
